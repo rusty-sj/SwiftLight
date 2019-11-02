6be6328d287a977a408eb460839d2b5719825427
Rashmi Jadhav <rsjcoepit@gmail.com>
2019-11-02 12:58:34 -0700

    Initial Commit

---
+++ SwiftLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLight
+//
+//  Created by Rashmi Jadhav on 11/2/19.
+//  Copyright © 2019 Rashmi Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftLightTests/SwiftLightTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftLightTests.swift
+//  SwiftLightTests
+//
+//  Created by Rashmi Jadhav on 11/2/19.
+//  Copyright © 2019 Rashmi Jadhav. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftLight
+
+class SwiftLightTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
